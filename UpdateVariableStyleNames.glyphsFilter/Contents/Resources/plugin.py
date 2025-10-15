4fe0982155cc5132d447aa11c1131d40d1c368f0
Hugo Jourdan <76793951+HugoJourdan@users.noreply.github.com>
2025-10-15 16:05:27 +0200

    Fix extra space issue

--- UpdateVariableStyleNames.glyphsFilter/Contents/Resources/plugin.py
+++ UpdateVariableStyleNames.glyphsFilter/Contents/Resources/plugin.py
@@ -3,10 +3,10 @@
 ###########################################################################################################
 #
 #
-#	Filter without dialog plug-in
+# Filter without dialog plug-in
 #
-#	Read the docs:
-#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Filter%20without%20Dialog
+# Read the docs:
+# https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Filter%20without%20Dialog
 #
 #
 ###########################################################################################################
@@ -18,31 +18,34 @@ from GlyphsApp.plugins import *
 
 
 class UpdateVariableStyleNames(FilterWithoutDialog):
-	
-	@objc.python_method
-	def settings(self):
-		self.menuName = Glyphs.localize({
-			"en": "Update Variable Style Names",
-			
-			})
+    @objc.python_method
+    def settings(self):
+        self.menuName = Glyphs.localize(
+            {
+                "en": "Update Variable Style Names",
+            }
+        )
 
-	@objc.python_method
-	def filter(self, layer, inEditView, customParameters):
-		
-		
-		elidable_prefix = customParameters[0] if customParameters else ""
-		font = layer.font()
+    @objc.python_method
+    def filter(self, layer, inEditView, customParameters):
+        elidable_prefix = customParameters[0] if customParameters else ""
+        print(elidable_prefix)
+        font = layer.font()
 
-		if layer.parent != font.glyphs[0]:
-			return
+        if layer.parent != font.glyphs[0]:
+            return
 
-		for instance in font.instances:
-			if instance.type == 0:
-				if instance.variableStyleName:
-					variable_style_name_renamed = instance.variableStyleName.replace(elidable_prefix, "").strip()
-					instance.variableStyleName = variable_style_name_renamed
+        for instance in font.instances:
+            if instance.type == 0:
+                if instance.variableStyleName:
+                    list_item = str(instance.variableStyleName).split()
+                    if elidable_prefix in list_item:
+                        list_item.remove(elidable_prefix)
+                    variable_style_name_renamed = " ".join(list_item)
 
-	@objc.python_method
-	def __file__(self):
-		"""Please leave this method unchanged"""
-		return __file__
+                    instance.variableStyleName = variable_style_name_renamed
+
+    @objc.python_method
+    def __file__(self):
+        """Please leave this method unchanged"""
+        return __file__
